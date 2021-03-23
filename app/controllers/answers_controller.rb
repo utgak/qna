8caf6321231d126a5e49776e21658be0b3313d6a
utgak <degtyaryov.2002@mail.ru>
2021-03-24 02:29:26 +0300

    fix

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,33 @@
+class AnswersController < ApplicationController
+  before_action :find_question, only: %i[create new]
+  before_action :find_answer, only: :show
+
+  def show; end
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to question_answers_path(@answer)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit( :body)
+  end
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def find_answer
+    @answer = Answer.find(params[:id])
+  end
+end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :answer do
     question
     body { 'MyString' }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
